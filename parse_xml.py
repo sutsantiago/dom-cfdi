3c433fe466c4f013c15fc863c26fa36cd4bb98ff
thomgonzalez <thom.sgonzalez@gmail.com>
2015-08-27 17:20:43 -0500

    Creación de propiedades de la clase ParseXML.

--- parse_xml.py
+++ parse_xml.py
@@ -6,16 +6,28 @@ class Base(object):
 	def __init__(self):
 		self._filename = self.file_name
 
-	def to_string(self):
+	def tostring(self):
 		tree = ET.parse(self._filename)
 		root = tree.getroot()
 		str_file = root.attrib
 		return str_file
 
+	def list_tostring(self):
+		return None
+
 
 class ParseXML(Base):
 	file_name = 'C:/Users/TomaS/repositorio/thomgonzalez/minidom/cfdi_test.xml'
 
-xml = ParseXML().to_string()
+	@property
+	def simple(self):
+		return self.tostring()
+
+	@property
+	def multiple(self):
+		return self.list_tostring()
+
+
+xml = ParseXML().simple
 print(xml)
 		
