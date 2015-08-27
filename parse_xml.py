3c8df318ee9e186b4fcd5d16f016df211a7f3bec
thomgonzalez <thom.sgonzalez@gmail.com>
2015-08-27 17:43:24 -0500

    Funcionalidad para lista de archivos xml y controlar excepciones.

--- parse_xml.py
+++ parse_xml.py
@@ -3,21 +3,37 @@ import xml.etree.ElementTree as ET
 
 class Base(object):
 
-	def __init__(self):
-		self._filename = self.file_name
+	def __init__(self, file_names=None):
+		self._filename = file_names
 
 	def tostring(self):
-		tree = ET.parse(self._filename)
-		root = tree.getroot()
-		str_file = root.attrib
-		return str_file
+		try:
+			tree = ET.parse(self._filename)
+			root = tree.getroot()
+			str_file = root.attrib
+		except Exception as e:
+			print("Error: "+str(e))
+		finally:
+			return str_file
 
 	def list_tostring(self):
-		return None
+		list_files = []
+		try:
+			for item in self._filename:
+				tree = ET.parse(item['name'])
+				root = tree.getroot()
+				str_file = root.attrib
+				list_files.append(str_file)
+		except Exception as e:
+			print("Error: "+str(e))
+		finally:
+			return list_files
 
 
 class ParseXML(Base):
-	file_name = 'C:/Users/TomaS/repositorio/thomgonzalez/minidom/cfdi_test.xml'
+	
+	def __init__(self, **kwargs):
+		super(ParseXML, self).__init__(**kwargs)
 
 	@property
 	def simple(self):
@@ -27,7 +43,14 @@ class ParseXML(Base):
 	def multiple(self):
 		return self.list_tostring()
 
+file_name = 'C:/Users/TomaS/repositorio/thomgonzalez/minidom/cfdi_test.xml'
+files = [
+	{'name':'C:/Users/TomaS/repositorio/thomgonzalez/minidom/cfdi_test.xml'},
+	{'name':'C:/Users/TomaS/repositorio/thomgonzalez/minidom/cfdi_test.xml'}
+]
 
-xml = ParseXML().simple
+xml = ParseXML(file_names=file_name).simple
+xmls = ParseXML(file_names=files).multiple
+
 print(xml)
-		
+print(xmls)
